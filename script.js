2fb008779793c01de7f62659933ccb3843499d6f
Akimoto-Arcan <49054149+Akimoto-Arcan@users.noreply.github.com>
2024-03-31 15:10:47 -0400

    Update script.js
    added in the trigger for the audio

--- script.js
+++ script.js
@@ -3,3 +3,11 @@ document.getElementById('rsvp-form').addEventListener('submit', function(event) 
   alert('Thank you for submitting your RSVP!');
   // Here you would add functionality to post data to a service like Google Forms or Airtable
 });
+
+document.body.addEventListener('click', function playMusic() {
+  var weddingMusic = document.getElementById('weddingMusic');
+  if (weddingMusic) {
+    weddingMusic.play();
+  }
+  document.body.removeEventListener('click', playMusic);
+}, {once: true});
