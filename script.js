c49a1f58af5a9798b1d20c4f97550c26a7762acb
Akimoto-Arcan <49054149+Akimoto-Arcan@users.noreply.github.com>
2024-03-31 15:02:14 -0400

    Create script.js

---
+++ script.js
@@ -0,0 +1,5 @@
+document.getElementById('rsvp-form').addEventListener('submit', function(event) {
+  event.preventDefault();
+  alert('Thank you for submitting your RSVP!');
+  // Here you would add functionality to post data to a service like Google Forms or Airtable
+});
